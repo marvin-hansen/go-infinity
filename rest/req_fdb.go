475607a6a087241e755f855ef8432ed40fd549d2
Marvin Hansen <marvin.hansen@gmail.com>
2022-03-06 20:13:48 +0800

    Added docstring.

--- rest/req_fdb.go
+++ rest/req_fdb.go
@@ -2,6 +2,7 @@ package rest
 
 import "go-infinity/rest/fdb"
 
+// DBPATH Retrieves the path to the boot file for the current federated database.
 func (c *Client) DBPATH(req *fdb.RequestForBootPath) (*fdb.ResponseForBootPath, error) {
 	results := fdb.NewResponseForBootPath()
 	if err := c.request(req, results); err != nil {
